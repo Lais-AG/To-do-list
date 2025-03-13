4516c53b8440d657b7a2d4b1281a9b9bb6208cd2
Lais Almeida Guedes <laisalmeida.saude@hotmail.com>
2025-03-13 19:31:07 +0000

    configuraçao de armazenamento local

--- script.js
+++ script.js
@@ -2,13 +2,15 @@ let defaulTheme = document.querySelector(".default-theme");
 
 let body = document.body;
 let title = document.querySelector(".task-title");
-let backgroundButton = document.querySelector(".task-button");
+let inputAdd = document.querySelector(".task-input");
+let buttonAdd = document.querySelector(".task-button");
+let taskList = document.querySelector(".task-list");
 
 defaulTheme.addEventListener("click", function(){
    body.style.background = "linear-gradient(100deg, #575656, #062e3f)";
    title.style.color = "#ffdfdb";
-   backgroundButton.style.backgroundColor = "#ffdfdb";
-   backgroundButton.style.color = "#062e3f";
+   buttonAdd.style.backgroundColor = "#ffdfdb";
+   buttonAdd.style.color = "#062e3f";
 });
 
 let lightTheme = document.querySelector(".light-theme");
@@ -16,8 +18,8 @@ let lightTheme = document.querySelector(".light-theme");
 lightTheme.addEventListener("click", function(){
     body.style.background = "linear-gradient(100deg, #FAFAFA, #ffffff)";
     title.style.color = "#1f1e1e";
-    backgroundButton.style.backgroundColor = "#1f1e1e";
-    backgroundButton.style.color = " #FAFAFA";
+    buttonAdd.style.backgroundColor = "#1f1e1e";
+    buttonAdd.style.color = " #FAFAFA";
 })
 
 let darkTheme = document.querySelector(".dark-theme");
@@ -25,17 +27,52 @@ let darkTheme = document.querySelector(".dark-theme");
 darkTheme.addEventListener("click", function(){
     body.style.background = "linear-gradient(100deg, #001214,#1d1d1d)";
     title.style.color = "#F8F8FF";
-    backgroundButton.style.backgroundColor = "#1C1C1C";
-    backgroundButton.style.color = "#F8F8FF";
-    backgroundButton.style.border = "1px solid #F8F8FF";
+    buttonAdd.style.backgroundColor = "#1C1C1C";
+    buttonAdd.style.color = "#F8F8FF";
+    buttonAdd.style.border = "1px solid #F8F8FF";
 });
 
 
 //sempre que estiver usando caixa de texto, uso o .VALUE
-backgroundButton.addEventListener("click", function(){
-    let taskInput = document.querySelector(".task-input").value;
-    document.querySelector(".task-list").innerHTML = taskInput;
+buttonAdd.addEventListener("click", function(){
+   let taskText = inputAdd.value.trim();
+
+
+   // !== (diferente)
+   //! (negaçao)
+   
+   if(taskText !== " "){
+    let itemList = document.createElement("li");
+    itemList.textContent = taskText;
+    taskList.appendChild(itemList);
+
+    let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    tasks.push(taskText)
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    inputAdd.value = " ";
+   }
 })
+
+//Localstorage armazena informaçoes em objetos.
+//variavel = um valor
+//array = +1 valor
+// objeto = +1 de valor mas de forma mais estruturada, armazena informaçoes, usando chave e valor
+
+window.addEventListener('load', function(){
+    let savedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+    savedTasks.forEach((task)=>{
+        let li = document.createElement("li");
+        li.textContent = task;
+        taskList.appendChild(li);
+
+        let removeButton = document.createElement("button");
+        removeButton.textContent = "remover";
+        li.appendChild(removeButton);
+
+        })
+})
+
 
 
 
